c75d7777605e1cad6af7658a0c2da82f19511efd
EC2 Default User <ec2-user@ip-172-31-3-2.us-east-2.compute.internal>
2018-02-02 12:05:22 +0000

    want

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,33 +5,23 @@ class ItemsController < ApplicationController
     @items = []
 
     @keyword = params[:keyword]
-    if @keyword.present?  
+    if @keyword.present? # 
       results = RakutenWebService::Ichiba::Item.search({
         keyword: @keyword,
         imageFlag: 1,
         hits: 20,
       })
-        #扱いやすいようにItemとしてインスタンスを作成する(保存はしない)
+      
+        #扱いやすいようにItemとしてインスタンスを作成する。(保存はしない)
       results.each do |result|
         item = Item.find_or_initialize_by(read(result))
-        @items << item #itemに[]を追加
+        @items << item
       end
     end
   end
-
-  private
-
-  def read(result)
-    code = result['itemCode']
-    name = result['itemName']
-    url = result['itemUrl']
-    image_url = result['mediumImageUrls'].first['imageUrl'].gsub('?_ex=128x128', '')
-
-    return {
-      code: code,
-      name: name,
-      url: url,
-      image_url: image_url,
-    }
+  
+  def show
+    @item = Item.find(params[:id])
+    @want_users = @item.want_users
   end
 end
---
+++ app/models/ownership.rb
@@ -0,0 +1,5 @@
+class Ownership < ApplicationRecord
+  belongs_to :user
+  belongs_to :item
+  
+end
