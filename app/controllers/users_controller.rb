c300554d083607cbeef8d042bc1f4983ca17edad
EC2 Default User <ec2-user@ip-172-31-3-2.us-east-2.compute.internal>
2018-02-04 06:49:11 +0000

    have function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,20 +5,28 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @items = @user.items.uniq
     @count_want = @user.want_items.count
+    @count_have = @user.have_items.count
   end
 
   def new
     @user = User.new
-    
+  end
+
+  def create
+    @user = User.new(user_params)
+
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
       redirect_to @user
     else
-      flash[:danger] = 'ユーザの登録に失敗しました。'
+      flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
     end
   end
 
-  def create
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
